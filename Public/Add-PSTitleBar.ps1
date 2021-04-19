e0018be4e2298771ef51010833a11f44a9c05caa
Tostka <tostka@users.noreply.github.com>
2021-04-19 16:06:41 -0500

    verb-io v1.0.61
        * 3:11 PM 4/19/2021 added cmdletbinding/verbose supp & alias add-PSTitle
         * 3:14 PM 4/19/2021 init vers

--- Public/Add-PSTitleBar.ps1
+++ Public/Add-PSTitleBar.ps1
@@ -14,6 +14,7 @@
     Github      : 
     Tags        : Powershell,Console
     REVISIONS
+    * 3:11 PM 4/19/2021 added cmdletbinding/verbose supp & alias add-PSTitle
     * 4:37 PM 2/27/2020 updated CBH
     # 8:44 AM 3/15/2017 ren Update-PSTitleBar => Add-PSTitleBar, so that we can have a Remove-PSTitleBar, to subtract
     # 8:42 AM 3/15/2017 Add-PSTitleBar for addition, before adding
@@ -29,6 +30,8 @@
     .LINK
     https://github.com/dsolodow/IndyPoSH/blob/master/Profile.ps1
     #>
+    [CmdletBinding()]
+    [Alias('add-PSTitle')]
     Param ([parameter(Mandatory = $true,Position=0)][String]$Tag)
     #only use on console host; since ISE shares the WindowTitle across multiple tabs, this information is misleading in the ISE.
     If ($host.name -eq 'ConsoleHost') {
---
+++ Public/set-PSTitleBar.ps1
@@ -0,0 +1,56 @@
+﻿#*------v Function set-PSTitleBar v------
+Function set-PSTitleBar {
+    <#
+    .SYNOPSIS
+    set-PSTitleBar.ps1 - Set specified powershell console Titlebar
+    .NOTES
+    Version     : 1.0.0
+    Author      : Todd Kadrie
+    Website     :	http://www.toddomation.com
+    Twitter     :	@tostka / http://twitter.com/tostka
+    CreatedDate : 2021-04-19
+    FileName    : set-PSTitleBar.ps1
+    License     : MIT License
+    Copyright   : (c) 2020 Todd Kadrie
+    Github      : https://github.com/tostka/verb-IO
+    Tags        : PowershellConsole
+    AddedCredit : mdjxkln
+    AddedWebsite:	https://xkln.net/blog/putting-the-powershell-window-title-to-better-use/
+    REVISIONS
+       * 3:14 PM 4/19/2021 init vers
+    .DESCRIPTION
+    set-PSTitleBar.ps1 - Set specified powershell console Titlebar
+    Added some examples posted by mdjxkln
+    .PARAMETER Title
+    Title string to be set on current powershell console Titlebar
+    .EXAMPLE
+    set-PSTitleBar 'EMS'
+    Set the string 'EMS' as the powershell console Title Bar
+    .EXAMPLE
+    if ((Get-History).Count -gt 0) {
+        set-PsTitleBar ((Get-History)[-1].CommandLine[0..25] -join '')
+    }
+    mdjxkln's example to set the title to the last entered command
+    .EXAMPLE
+    $ExampleArray = @(1..200) ; 
+    $ExampleArray | % {
+        Write-Host "Processing $_" # Doing some stuff...
+        $PercentProcessed = [Math]::Round(($ExampleArray.indexOf($_) + 1) / $ExampleArray.Count * 100,0) ; 
+        $Host.UI.RawUI.WindowTitle = "$PercentProcessed% Completed" ; 
+        Start-Sleep -Milliseconds 50 ; 
+    } ; 
+    mdjxkln's example to display script progress
+    .LINK
+    https://xkln.net/blog/putting-the-powershell-window-title-to-better-use/
+    .LINK
+    https://github.com/tostka/verb-IO
+    #>
+    [CmdletBinding()]
+    [Alias('set-PSTitle')]
+    Param ([parameter(Mandatory = $true,Position=0,HelpMessage="Title string to be set on current powershell console Titlebar[-title 'PS Window'")][String]$Title)
+    If ($host.name -eq 'ConsoleHost') {
+        #only use on console host; since ISE shares the WindowTitle across multiple tabs
+        $host.ui.RawUI.WindowTitle = $Title ;
+    } ;
+} ; 
+#*------^ END Function set-PSTitleBar ^------
