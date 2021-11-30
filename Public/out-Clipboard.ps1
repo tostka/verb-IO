808c7733052c72b753575476db558329837f937e
Tostka <tostka@users.noreply.github.com>
2021-11-30 16:35:30 -0600

    20211130-1635PM:EOD closing commit

--- Public/out-Clipboard.ps1
+++ Public/out-Clipboard.ps1
@@ -1,4 +1,4 @@
-#*------v Function out-Clipboard v------
+#*------v out-Clipboard.ps1 v------
 Function out-Clipboard {
     <#
     .SYNOPSIS
@@ -18,10 +18,10 @@ Function out-Clipboard {
     AddedWebsite:	https://community.idera.com/members/tobias-weltner
     AddedTwitter:	URL
     REVISIONS
+    * 10:59 AM 11/29/2021 fixed - shift to adv func broke the $input a-vari (only present w simple funcs): Added declared $content pipeline vari; added -NoLegacy switch to suppress the default 'append-`n to each line' clip.exe behavior emulation. 
     * 3:17 PM 11/8/2021 init vers, flip profile alias & clip.exe to holistic function for either
     .DESCRIPTION
     out-Clipboard.ps1 - cross-version emulation of the older pre-psv3 out-clipboard 'clip.exe' use (for pre psv3); or emulates the `n-appending bahavior of clip.exe, when using it for psv3+. This differentiates from the native set-clipboard, which appends no trailing `n.
-    
     Set-clipboard supports pipeline support, like the older | clip.exe approach. 
     But, there are differences between set-clipboard & clip.exe: 
     - clip.exe appends `n to every item added. 
@@ -32,25 +32,40 @@ Function out-Clipboard {
     .EXAMPLE
     "some text" | out-clipboard ; 
     .LINK
-    https://community.idera.com/database-tools/powershell/powertips/b/tips/posts/converting-hash-tables-to-objects
-    .LINK
     https://github.com/tostka/verb-IO
     #>
 
     [CmdletBinding()]
-    Param ()
+    Param (
+        [Parameter(Position=0,Mandatory=$True,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true,HelpMessage="Content to be copied to clipboard [-Content `$object]")]
+        [ValidateNotNullOrEmpty()]$Content,
+        [Parameter(HelpMessage="Switch to suppress the default 'append `n' clip.exe-emulating behavior[-NoLegacy]")]
+        [switch]$NoLegacy
+    ) ;
     PROCESS {
         if($host.version.major -lt 3){
             # provide clipfunction downrev
-            $tClip = "$((Resolve-Path $env:SystemRoot\System32\clip.exe).path)" ;
-            #$input | "($tClip)" ; 
-            Set-Alias -Name 'Out-Clipboard' -Value $tClip -scope script ;
-            $input | out-clipboard 
+            if(-not (get-command out-clipboard)){
+                # build the alias if not pre-existing
+                $tClip = "$((Resolve-Path $env:SystemRoot\System32\clip.exe).path)" ;
+                #$input | "($tClip)" ; 
+                #$content | ($tClip) ; 
+                Set-Alias -Name 'Out-Clipboard' -Value $tClip -scope script ;
+            } ;
+            # input only works in simple functions, in adv funcs declare a suitable vari
+            #$input | out-clipboard 
+            $content | out-clipboard ;
         } else {
             # emulate clip.exe's `n-append behavior on ps3+
-            $input = $input | foreach-object {"$($_)$([Environment]::NewLine)"} ; 
+            <#$input = $input | foreach-object {"$($_)$([Environment]::NewLine)"} ; 
             $input | set-clipboard ; 
+            #>
+            if(-not $NoLegacy){
+                $content = $content | foreach-object {"$($_)$([Environment]::NewLine)"} ; 
+            } ; 
+            $content | set-clipboard ;
         } ; 
     } ; 
-} ; 
-#*------^ END Function out-Clipboard ^------
+}
+
+#*------^ out-Clipboard.ps1 ^------
