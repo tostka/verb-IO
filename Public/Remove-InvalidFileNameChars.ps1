16733f3449011406d6ae20727ddd1ad23d0210c6
Tostka <tostka@users.noreply.github.com>
2021-12-20 16:12:27 -0600

    Update Remove-InvalidFileNameChars.ps1
    * 4:35 PM 12/16/2021 added -PurgeSpaces, to fully strip down the result. Added a 2nd CBH example, need to rebuild verb-IO

--- Public/Remove-InvalidFileNameChars.ps1
+++ Public/Remove-InvalidFileNameChars.ps1
@@ -17,18 +17,28 @@ Function Remove-InvalidFileNameChars {
     Github      : https://github.com/tostka/verb-IO
     Tags        : Powershell,Filesystem
     REVISIONS   :
+    * 4:35 PM 12/16/2021 added -PurgeSpaces, to fully strip down the result. Added a 2nd CBH example
     * 7:21 AM 9/2/2020 added alias:'Remove-IllegalFileNameChars'
     * 3:32 PM 9/1/2020 added to verb-IO
     * 4/14/14 posted version
     .DESCRIPTION
     Remove-InvalidFileNameChars - Remove OS-specific illegal filename characters from the passed string
+    Note: You should pass the filename, and not a full-path specification as '-Name', 
+    or the function will remove path-delimters and other routine path components. 
     .PARAMETER Name
+    Potential file 'name' string (*not* path), to have illegal filename characters removed. 
+    .PARAMETER PurgeSpaces
+    Switch to purge spaces along with OS-specific illegal filename characters. 
     .INPUTS
     Accepts piped input.
     .OUTPUTS
     System.String
     .EXAMPLE
     $Name = Remove-InvalidFileNameChars -name $ofile ; 
+    Remove OS-specific illegal characters from the sample filename in $ofile. 
+    .EXAMPLE
+    $Name = Remove-InvalidFileNameChars -name $ofile -purgespaces ; 
+    Remove OS-specific illegal characters & spaces from the sample filename in $ofile. 
     .LINK
     https://github.com/tostka/verb-IO
     #>
@@ -36,10 +46,15 @@ Function Remove-InvalidFileNameChars {
     [Alias('Remove-IllegalFileNameChars')]
     Param(
         [Parameter(Mandatory=$true,Position=0,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)]
-        [String]$Name
+        [String]$Name,
+        [switch]$PurgeSpaces
     )
     $verbose = ($VerbosePreference -eq "Continue") ; 
-    $invalidChars = [IO.Path]::GetInvalidFileNameChars() -join ''
+    $invalidChars = [IO.Path]::GetInvalidFileNameChars() -join '' ; 
+    if($PurgeSpaces){
+        write-verbose "(-PurgeSpaces: removing spaces as well)" ; 
+        $invalidChars += ' ' ; 
+    } ; 
     $re = "[{0}]" -f [RegEx]::Escape($invalidChars)
     ($Name -replace $re) | write-output ; 
 }#*------^ END Function Remove-InvalidFileNameChars ^------ ;
