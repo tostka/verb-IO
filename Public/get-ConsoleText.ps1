804bf73263e51305ccc02bb393cce9d44c947ca8
Tostka <tostka@users.noreply.github.com>
2022-02-02 12:15:09 -0600

    vio v1.6.3: get-ConsoleText
    get-ConsoleText: * 9:27 AM 2/2/2022 added -topipeline switch & post-split code (pipeline return returns text block, not lines, and os-agnostic split pads with spaces between lines unless explicitly supressed); fixed output: was just dumping console text to pipeline (and back into console buffer), piped it into set-clipboard ; minor tweaks OTB fmt, added CBH ; added else clause to echo non-support of VsCode.
        * 6/5/2019 posted AutomatedLab revision (non-functional, doesn't copy to cb)

--- Public/get-ConsoleText.ps1
+++ Public/get-ConsoleText.ps1
@@ -8,7 +8,7 @@ Function get-ConsoleText {
     Author      : Todd Kadrie
     Website     :	http://www.toddomation.com
     Twitter     :	@tostka / http://twitter.com/tostka
-    AddedCredit : AutomatedLab:Raimund Andrée [MSFT],Jan-Hendrik Peters [MSFT] 
+    AddedCredit : AutomatedLab:Raimund Andrée [MSFT],Jan-Hendrik Peters [MSFT]
     AddedWebsite:	https://github.com/AutomatedLab/AutomatedLab.Common/blob/develop/AutomatedLab.Common/Common/Public/Get-ConsoleText.ps1
     AddedGithub : https://github.com/AutomatedLab/AutomatedLab
     AddedTwitter:	@raimundandree,@nyanhp
@@ -19,51 +19,84 @@ Function get-ConsoleText {
     Github      : https://github.com/tostka/verb-IO
     Tags        : PowershellConsole,Clipboard,Text
     REVISIONS
-    * 8:01 AM 2/2/2022 fixed output: was just dumping console text to pipeline (and back into console buffer), piped it into set-clipboard ; minor tweaks OTB fmt, added CBH ; 
-    * 6/5/2019 posted revision
+    * 9:27 AM 2/2/2022 added -topipeline switch & post-split code (pipeline return returns text block, not lines, and os-agnostic split pads with spaces between lines unless explicitly supressed); fixed output: was just dumping console text to pipeline (and back into console buffer), piped it into set-clipboard ; minor tweaks OTB fmt, added CBH ; added else clause to echo non-support of VsCode.
+    * 6/5/2019 posted AutomatedLab revision (non-functional, doesn't copy to cb)
     .DESCRIPTION
     get-ConsoleText.ps1 - Copies current powershell console buffer to the clipboard
+    .PARAMETER toPipeline
+    switch to return the text to the pipeline (rather than default 'copy to clipboard' behavior)[-toPipeline]
     .OUTPUT
-    None. Outputs console text to clipboard. 
+    None. Outputs console text to clipboard.
     .EXAMPLE
-    get-ConsoleText ; 
-    get-clipboard |  measure | select -expand count ; 
+    get-ConsoleText ;
+    get-clipboard |  measure | select -expand count ;
     Copy console text to clipboard, then output the number of lines returned.
+    .EXAMPLE
+    $content = get-ConsoleText -toPipeline ;
+    $content.Split([Environment]::NewLine, [StringSplitOptions]::RemoveEmptyEntries) |  measure | select -expand count ; 
+    $content.Split([Environment]::NewLine, [StringSplitOptions]::RemoveEmptyEntries)) | select -first 15 ; 
+    Demonstrate assign console text to a variable, and resplit (suppressing empty lines), and output the first 15 lines returned.
     .LINK
     https://github.com/AutomatedLab/AutomatedLab.Common/blob/develop/AutomatedLab.Common/Common/Public/Get-ConsoleText.ps1
     .LINK
     https://github.com/tostka/verb-IO
     #>
     [Alias('tmf')]
-    PARAM() ;
-    # Check the host name and exit if the host is not the Windows PowerShell console host. 
-    if ($host.Name -eq 'Windows PowerShell ISE Host') { 
-        $psISE.CurrentPowerShellTab.ConsolePane.Text ; 
+    PARAM(
+        [Parameter(HelpMessage="switch to return the text to the pipeline (rather than default 'copy to clipboard' behavior)[-toPipeline]")]
+        [switch] $toPipeline
+    ) ;
+    
+    # Check the host name and exit if the host is not the Windows PowerShell console host.
+    if ($host.Name -eq 'Windows PowerShell ISE Host') {
+        write-verbose "(`$host.Name:Windows PowerShell ISE Host detected)" ;
+        write-verbose "(copying to cb...)" ;
+        $return = $psISE.CurrentPowerShellTab.ConsolePane.Text ; 
     } elseif ($host.Name -eq 'ConsoleHost') {
-        $textBuilderConsole = New-Object System.Text.StringBuilder ; 
-        $textBuilderLine = New-Object System.Text.StringBuilder ; 
+        write-verbose "(`$host.Name:ConsoleHost detected)" ;
+        $textBuilderConsole = New-Object System.Text.StringBuilder ;
+        $textBuilderLine = New-Object System.Text.StringBuilder ;
 
         # Grab the console screen buffer contents using the Host console API.
-        $bufferWidth = $host.UI.RawUI.BufferSize.Width ; 
-        $bufferHeight = $host.UI.RawUI.CursorPosition.Y  ; 
-        $rec = New-Object System.Management.Automation.Host.Rectangle(0, 0, ($bufferWidth), $bufferHeight) ; 
-        $buffer = $host.UI.RawUI.GetBufferContents($rec)  ; 
-        
+        $bufferWidth = $host.UI.RawUI.BufferSize.Width ;
+        $bufferHeight = $host.UI.RawUI.CursorPosition.Y  ;
+        $rec = New-Object System.Management.Automation.Host.Rectangle(0, 0, ($bufferWidth), $bufferHeight) ;
+        $buffer = $host.UI.RawUI.GetBufferContents($rec)  ;
+
         #Console buffer actually stores console formatting, along with characters
         # getting text out requires some processing of the raw content
-        # Iterate through the lines in the console buffer. 
-        for ($i = 0; $i -lt $bufferHeight; $i++) { 
-            for ($j = 0; $j -lt $bufferWidth; $j++) { 
-                $cell = $buffer[$i, $j]  ; 
-                $null = $textBuilderLine.Append($cell.Character) ; 
-            } ; 
-            $null = $textBuilderConsole.AppendLine($textBuilderLine.ToString().TrimEnd()) ; 
-            $textBuilderLine = New-Object System.Text.StringBuilder ; 
-        } ; 
-        # erm, below wasn't copying to console, it was dumping to pipeline! (effectively dumping same text back into console)
-        #$textBuilderConsole.ToString() ; 
-        $textBuilderConsole.ToString() | set-clipboard ; 
-        Write-Verbose "$bufferHeight lines have been copied to the clipboard" ; 
-    } ;     
-} ; 
+        # Iterate through the lines in the console buffer.
+        write-verbose  "(processing buffer)" ; 
+        for ($i = 0; $i -lt $bufferHeight; $i++) {
+            for ($j = 0; $j -lt $bufferWidth; $j++) {
+                $cell = $buffer[$i, $j]  ;
+                $null = $textBuilderLine.Append($cell.Character) ;
+            } ;
+            $null = $textBuilderConsole.AppendLine($textBuilderLine.ToString().TrimEnd()) ;
+            $textBuilderLine = New-Object System.Text.StringBuilder ;
+        } ;
+        # original code was dropping into pipeline, not copying to clipboard (as per echos). 
+        $return = $textBuilderConsole.ToString() ;
+        Write-Verbose "$bufferHeight lines have been processed" ;
+    } elseif( $env:TERM_PROGRAM -eq 'vscode' ){
+        write-warning "(VSCode detected: unsupported)" ; 
+        BREAK ;
+    } else {
+        write-warning "(unrecognized `$host.Name:$($host.name))" ; 
+        BREAK ;
+    } ; 
+    if($return -AND -not $toPipeline){
+        write-verbose "(copied content to clipboard)" ; 
+        # $content 
+        $return | set-clipboard
+    } else { 
+        write-verbose "(returning content to pipeline)" ; 
+        # this writes everyting as a single text block, unsplit
+        #$return | write-output ; 
+        # split before returning - this natively suppresses emtpy lines
+        #$return.Split(@("`r`n", "`r", "`n"),[StringSplitOptions]::None) | write-output ; 
+        # variant, OS agnostic, requires removeemptyentries or it leaves gaps (inflates a 3k line console to 6k lines). 
+        $return.Split([Environment]::NewLine, [StringSplitOptions]::RemoveEmptyEntries) ;
+    } ; 
+} ;
 #*------^ get-ConsoleText.ps1 ^------
