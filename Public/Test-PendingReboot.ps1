09f777410006627c085060605f3b7425e68b9b13
Tostka <tostka@users.noreply.github.com>
2022-05-04 16:13:30 -0500

    Update Test-PendingReboot.ps1
    * 1:35 PM 4/25/2022 psv2 explcit param property =$true; regexpattern w single quotes.

--- Public/Test-PendingReboot.ps1
+++ Public/Test-PendingReboot.ps1
@@ -1,4 +1,4 @@
-#*------v Function Test-PendingReboot v------
+#*------v Test-PendingReboot.ps1 v------
 function Test-PendingReboot {
     <#
     .SYNOPSIS
@@ -18,6 +18,7 @@ function Test-PendingReboot {
     Github      : https://github.com/tostka/verb-XXX
     Tags        : Powershell,System,Reboot
     REVISIONS
+    * 1:35 PM 4/25/2022 psv2 explcit param property =$true; regexpattern w single quotes.
     * 9:52 AM 2/11/2021 pulled spurios trailing fire of the cmd below func, updated CBH
     * 5:03 PM 1/14/2021 init, minor CBH mods
     * 7/29/19 AB's posted version
@@ -27,7 +28,7 @@ function Test-PendingReboot {
     Array of computernames to be tested for pending reboot
     .PARAMETER  Credential
     windows Credential [-credential (get-credential)]
-    .OUTPUT 
+    .OUTPUT
     System.Collections.Hashtable
     .EXAMPLE
     if((Test-PendingReboot -ComputerName $env:Computername).IsPendingReboot){write-warning "$env:computername is PENDING REBOOT!"} ;
@@ -38,7 +39,7 @@ function Test-PendingReboot {
     [CmdletBinding()]
     #[Alias('get-ScheduledTaskReport')]
     PARAM(
-        [Parameter(Mandatory)]
+        [Parameter(Mandatory=$true)]
         [ValidateNotNullOrEmpty()]
         [string[]]$ComputerName,
         [Parameter()]
@@ -50,8 +51,8 @@ function Test-PendingReboot {
     $scriptBlock = {
 
         $VerbosePreference = $using:VerbosePreference
-        
-        <# try to pull in local modules into the scriptblock (rather than expliciting a copy in the block) - didn't work, module load threw 
+
+        <# try to pull in local modules into the scriptblock (rather than expliciting a copy in the block) - didn't work, module load threw
         Error Message: A Using variable cannot be retrieved. A Using variable can be used only with Invoke-Command, Start-Job, or InlineScript in the script workflow. When it is used with Invoke-Command, the Using variable is valid only if the script block is invoked on a remote computer
         #>
         function Test-RegistryKey {
@@ -59,7 +60,7 @@ function Test-PendingReboot {
             [CmdletBinding()]
             param
             (
-                [Parameter(Mandatory)]
+                [Parameter(Mandatory=$true)]
                 [ValidateNotNullOrEmpty()]
                 [string]$Key
             )
@@ -73,10 +74,10 @@ function Test-PendingReboot {
             [CmdletBinding()]
             param
             (
-                [Parameter(Mandatory)]
+                [Parameter(Mandatory=$true)]
                 [ValidateNotNullOrEmpty()]
                 [string]$Key,
-                [Parameter(Mandatory)]
+                [Parameter(Mandatory=$true)]
                 [ValidateNotNullOrEmpty()]
                 [string]$Value
             )
@@ -90,10 +91,10 @@ function Test-PendingReboot {
             [CmdletBinding()]
             param
             (
-                [Parameter(Mandatory)]
+                [Parameter(Mandatory=$true)]
                 [ValidateNotNullOrEmpty()]
                 [string]$Key,
-                [Parameter(Mandatory)]
+                [Parameter(Mandatory=$true)]
                 [ValidateNotNullOrEmpty()]
                 [string]$Value
             )
@@ -114,10 +115,10 @@ function Test-PendingReboot {
             { Test-RegistryKey -Key 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\PostRebootReporting' }
             { Test-RegistryValueNotNull -Key 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager' -Value 'PendingFileRenameOperations' }
             { Test-RegistryValueNotNull -Key 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager' -Value 'PendingFileRenameOperations2' }
-            { 
+            {
                 # Added test to check first if key exists, using "ErrorAction ignore" will incorrectly return $true
-                'HKLM:\SOFTWARE\Microsoft\Updates' | Where-Object { test-path $_ -PathType Container } | ForEach-Object {            
-                    (Get-ItemProperty -Path $_ -Name 'UpdateExeVolatile' | Select-Object -ExpandProperty UpdateExeVolatile) -ne 0 
+                'HKLM:\SOFTWARE\Microsoft\Updates' | Where-Object { test-path $_ -PathType Container } | ForEach-Object {
+                    (Get-ItemProperty -Path $_ -Name 'UpdateExeVolatile' | Select-Object -ExpandProperty UpdateExeVolatile) -ne 0
                 }
             }
             { Test-RegistryValue -Key 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce' -Value 'DVDRebootSignal' }
@@ -127,12 +128,12 @@ function Test-PendingReboot {
             {
                 # Added test to check first if keys exists, if not each group will return $Null
                 # May need to evaluate what it means if one or both of these keys do not exist
-                ( 'HKLM:\SYSTEM\CurrentControlSet\Control\ComputerName\ActiveComputerName' | Where-Object { test-path $_ } | %{ (Get-ItemProperty -Path $_ ).ComputerName } ) -ne 
+                ( 'HKLM:\SYSTEM\CurrentControlSet\Control\ComputerName\ActiveComputerName' | Where-Object { test-path $_ } | %{ (Get-ItemProperty -Path $_ ).ComputerName } ) -ne
                 ( 'HKLM:\SYSTEM\CurrentControlSet\Control\ComputerName\ComputerName' | Where-Object { Test-Path $_ } | %{ (Get-ItemProperty -Path $_ ).ComputerName } )
             }
             {
                 # Added test to check first if key exists
-                'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Services\Pending' | Where-Object { 
+                'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Services\Pending' | Where-Object {
                     (Test-Path $_) -and (Get-ChildItem -Path $_) } | ForEach-Object { $true }
             }
         ) ; # scriptblock-E
@@ -148,22 +149,19 @@ function Test-PendingReboot {
 
     foreach ($computer in $ComputerName) {
         try {
-            $connParams = @{'ComputerName' = $computer} ; 
+            $connParams = @{'ComputerName' = $computer} ;
             if ($PSBoundParameters.ContainsKey('Credential')) {
-                $connParams.Credential = $Credential ; 
-            } ; 
-
+                $connParams.Credential = $Credential ;
+            } ;
             $output = @{
-                ComputerName    = $computer ; 
-                IsPendingReboot = $false ; 
-            } ; 
-
-            $psRemotingSession = New-PSSession @connParams ; 
-            
+                ComputerName    = $computer ;
+                IsPendingReboot = $false ;
+            } ;
+            $psRemotingSession = New-PSSession @connParams ;
             if (-not ($output.IsPendingReboot = Invoke-Command -Session $psRemotingSession -ScriptBlock $scriptBlock)) {
-                $output.IsPendingReboot = $false ; 
-            } ; 
-            [pscustomobject]$output | write-output ; 
+                $output.IsPendingReboot = $false ;
+            } ;
+            [pscustomobject]$output | write-output ;
         } catch {
             Write-Error -Message $_.Exception.Message
         } finally {
@@ -171,6 +169,7 @@ function Test-PendingReboot {
                 $psRemotingSession | Remove-PSSession
             }
         } # TRY-E
-    } ; 
-} ; 
-#*------^ END Function Test-PendingReboot ^------
+    } ;
+}
+
+#*------^ Test-PendingReboot.ps1 ^------
