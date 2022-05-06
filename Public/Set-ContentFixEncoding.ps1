454b1f8e4b5afb76ac1038d723089ac802db6f1a
Tostka <tostka@users.noreply.github.com>
2022-05-06 15:40:50 -0500

    Update Set-ContentFixEncoding.ps1
    3:40 PM 5/6/2022 added echo top 2 lines of passed Value, and added pswlt

--- Public/Set-ContentFixEncoding.ps1
+++ Public/Set-ContentFixEncoding.ps1
@@ -76,6 +76,10 @@ function Set-ContentFixEncoding {
             else{ write-verbose "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ; } ;
             # set-content overwrites target file, no need to pretest and attempt to retain, just force to encoding specified
             $pltSetCon = @{ Path=$Item.FullName ; encoding=$encoding ; ErrorAction = 'STOP' ; PassThru = $($PassThru); whatif=$($whatif) ;  } ;
+            $smsg = "Set-Content w`n$(($pltSetCon|out-string).trim())" ; 
+            $smsg += "`n-Value[0,2]:`n$(($value | out-string).Split([Environment]::NewLine, [StringSplitOptions]::RemoveEmptyEntries)| select -first 2|out-string)" ; 
+            if($verbose){ if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level Info }
+            else{ write-verbose "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ; } ;
             $Exit = 0 ;
             Do {
                 Try{
