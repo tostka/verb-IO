f821a808558363a16338e883cfea69a087b496b6
Tostka <tostka@users.noreply.github.com>
2022-05-27 17:21:01 -0500

    convert-DehydratedBytesToGB|MB.ps1
     * 3:08 PM 5/27/2022 CBH added example for select property expression; strongly typed output as double

--- Public/convert-DehydratedBytesToGB.ps1
+++ Public/convert-DehydratedBytesToGB.ps1
@@ -15,19 +15,38 @@ Function convert-DehydratedBytesToGB {
     Github      : https://github.com/tostka/verb-io
     Tags        : Powershell,Conversion,Storage,Unit
     REVISIONS
+    * 3:08 PM 5/27/2022 CBH added example for select property expression; strongly typed output as double
     * 1:34 PM 2/25/2022 refactored CBH (was broken, non-parsing), renamed -data -> -string, retained prior name as a parameter alias
     * 5:19 PM 7/20/2021 init vers
     .DESCRIPTION
-    convert-DehydratedBytesToGB - Convert MS Dehydrated byte sizes string - NN.NN MB (nnn,nnn,nnn bytes) - into equivelent decimal gigabytes.
+    convert-DehydratedBytesToGB - Convert MS Dehydrated byte sizes string - 'NN.NN MB (nnn,nnn,nnn bytes)' string returned - into equivelent decimal gigabytes.
+    Microsoft routinely returns storage values as space/parenthese-delimited string in units value, and a parenthetical comma'd bytes value. Neither of which is usable for comparison or sorting, unless converted to a single underlying unit of value. This does that conversion. 
     .PARAMETER String
     Array of Dehydrated byte sizes to be converted
     .PARAMETER Decimals
     Number of decimal places to return on results
+    .INPUTS
+    Accepts pipeline input. 
     .OUTPUTS
-    System.String
+    System.Double
     .EXAMPLE
     PS> (get-mailbox -id hoffmjj | get-mailboxstatistics).totalitemsize | convert-DehydratedBytesToGB ;
     Convert a series of get-MailboxStatistics.totalitemsize.values ("102.8 MB (107,808,015 bytes)") into decimal gigabyte values.
+    .EXAMPLE
+    PS> $propsFldr = 'Name','ItemsInFolder', @{Name='FolderSizeGB';Expression={$_.FolderSize | convert-DehydratedBytesToGB -decimals 5 }},@{Name='TopSubjectSizeGB';Expression={$_.TopSubjectSize | convert-DehydratedBytesToGB -decimals 5 }},'TopSubject','TopSubjectCount','TopSubjectClass' ; 
+    PS> Connect-ExchangeOnline ; 
+    PS> $fldrstats = Get-XOMailboxFolderStatistics -Identity user@domain.com  -IncludeAnalysis -FolderScope RecoverableItems ; 
+    PS>  $fldrstats | select $propsFldr |?{$_.TopSubjectSizeGB -gt 1}
+    
+    Name             : DiscoveryHolds
+    ItemsInFolder    : 291212
+    FolderSizeGB     : 98.03443
+    TopSubjectSizeGB : 49.32737
+    TopSubject       : Subject Meeting
+    TopSubjectCount  : 29154
+    TopSubjectClass  : IPM.Appointment
+    
+    Demo construction of a select-object properties array that includes expressions leveraging convert-DehydratedBytesToGB to produce decimal gigabyte sizes for EXO two dehydrated byte sizes properties (with 5-digit decimal values), and then postfiltering that value for oversize mailbox folders (with ExchangeOnlineManagement module)
     .LINK
     https://github.com/tostka/verb-IO
     #>
@@ -55,7 +74,7 @@ Function convert-DehydratedBytesToGB {
         If($String -match '.*\s\(.*\sbytes\)'){ 
             foreach($item in $String){
                 # replace ".*(" OR "\sbytes\).*" OR "," (with nothing, results in the raw bytes numeric value), then foreach and format to gb or mb decimal places (depending on tomb or togb variant of the function)
-                $item -replace '.*\(| bytes\).*|,' |foreach-object {$FmtCode  -f ($_ / 1GB)} | write-output ;
+                [double]($item -replace '.*\(| bytes\).*|,' |foreach-object {$FmtCode  -f ($_ / 1GB)}) | write-output ;
                 # sole difference between GB & MB funcs is the 1GB/1MB above
             } ; 
         } else { 
--- Public/convert-DehydratedBytesToMB.ps1
+++ Public/convert-DehydratedBytesToMB.ps1
@@ -2,7 +2,7 @@
 Function convert-DehydratedBytesToMB {
     <#
     .SYNOPSIS
-    convert-DehydratedBytesToMB - Convert MS Dehydrated byte sizes string - NN.NN MB (nnn,nnn,nnn bytes) - into equivelent decimal megabytes.
+    convert-DehydratedBytesToMB - Convert MS Dehydrated byte sizes string returned - 'NN.NN MB (nnn,nnn,nnn bytes)' - into equivelent decimal megabytes.
     .NOTES
     Version     : 1.0.0
     Author      : Todd Kadrie
@@ -15,19 +15,27 @@ Function convert-DehydratedBytesToMB {
     Github      : https://github.com/tostka/verb-io
     Tags        : Powershell,Conversion,Storage,Unit
     REVISIONS
+    * 3:08 PM 5/27/2022 CBH added example for select property expression; strongly typed output as double
     * 1:34 PM 2/25/2022 refactored CBH (was broken, non-parsing), renamed -data -> -string, retained prior name as a parameter alias
     * 5:19 PM 7/20/2021 init vers
     .DESCRIPTION
-    convert-DehydratedBytesToMB - Convert MS Dehydrated byte sizes string - NN.NN MB (nnn,nnn,nnn bytes) - into equivelent decimal megabytes.
+    convert-DehydratedBytesToMB - Convert MS Dehydrated byte sizes string returned - 'NN.NN MB (nnn,nnn,nnn bytes)' - into equivelent decimal megabytes.
     .PARAMETER String
     Array of Dehydrated byte sizes to be converted
     .PARAMETER Decimals
     Number of decimal places to return on results
+    .INPUTS
+    Accepts pipeline input. 
     .OUTPUTS
-    System.String
+    System.Double
     .EXAMPLE
     PS> (get-mailbox -id hoffmjj | get-mailboxstatistics).totalitemsize | convert-DehydratedBytesToMB ;
-    Convert a series of get-MailboxStatistics.totalitemsize.values ("102.8 MB (107,808,015 bytes)") into decimal gigabyte values.
+    Convert a series of get-MailboxStatistics.totalitemsize.values ("102.8 MB (107,808,015 bytes)") into decimal megabyte values.
+    .EXAMPLE
+    PS> $propsFldr = 'Name','ItemsInFolder', @{Name='FolderSizeMB';Expression={$_.FolderSize | convert-DehydratedBytesToMB -decimals 5 }},@{Name='TopSubjectSizeGB';Expression={$_.TopSubjectSize | convert-DehydratedBytesToGB -decimals 5 }},'TopSubject','TopSubjectCount','TopSubjectClass' ; 
+    PS> Connect-ExchangeOnline ; 
+    PS> $fldrstats = Get-XOMailboxFolderStatistics -Identity user@domain.com  -IncludeAnalysis -FolderScope RecoverableItems ; 
+    PS>  $fldrstats | select $propsFldr |?{$_.TopSubjectSizeMB -gt 1}
     .LINK
     https://github.com/tostka/verb-IO
     #>
@@ -55,7 +63,7 @@ Function convert-DehydratedBytesToMB {
         If($String -match '.*\s\(.*\sbytes\)'){ 
             foreach($item in $String){
                 # replace ".*(" OR "\sbytes\).*" OR "," (with nothing, results in the raw bytes numeric value), then foreach and format to gb or mb decimal places (depending on tomb or togb variant of the function)
-                $item -replace '.*\(| bytes\).*|,' |foreach-object {$FmtCode  -f ($_ / 1MB)} | write-output ;
+                [double]($item -replace '.*\(| bytes\).*|,' |foreach-object {$FmtCode  -f ($_ / 1MB)}) | write-output ;
                 # sole difference between GB & MB funcs is the 1GB/1MB above
             } ; 
         } else { 
