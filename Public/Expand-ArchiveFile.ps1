8e8ab33e932ad1c9471c2712ce2e5d5e187f96fa
Tostka <tostka@users.noreply.github.com>
2022-08-30 16:41:46 -0500

    Expand-ArchiveFile
    * 4:23 PM 8/30/2022 simplified CATCH's; updated CBH; ren -Destination -> -DestinationPath (matches expand-archive, and compress-archivefile params)

--- Public/Expand-ArchiveFile.ps1
+++ Public/Expand-ArchiveFile.ps1
@@ -2,7 +2,7 @@
 function Expand-ArchiveFile {
     <#
     .SYNOPSIS
-    Expand-ArchiveFile.ps1 - Decompress all files in an archive file to a destination directory
+    Expand-ArchiveFile.ps1 - Decompress all files in an archive file to a destination directory (wraps Psv5+ native cmdlets and matching legacy .net ZipFile & Shell.Application calls)
     .NOTES
     Author: Taylor Gibb
     Website:	https://www.howtogeek.com/tips/how-to-extract-zip-files-using-powershell/
@@ -11,22 +11,22 @@ function Expand-ArchiveFile {
     Twitter:	http://twitter.com/tostka
     Additional Credits:
     REVISIONS   :
+    * 4:23 PM 8/30/2022 simplified CATCH's; updated CBH; ren -Destination -> -DestinationPath (matches expand-archive, and compress-archivefile params)
     * 1:28 PM 8/29/2022 ren Expand-ZIPFile -> Expand-ArchiveFile (alias orig name); ren source parameter File -> Path; add code to use native expand-archive on psv5+ ; 
         added try/catch support; debugged on psv51 on mybox, pipeline & useshell (older revs untested).
     * 7:28 AM 3/14/2017 updated tsk: pshelp, param() block, OTB format
-    * 06/13/13 (posted version)
     .DESCRIPTION
-    Expand-ArchiveFile.ps1 - Decompress all files in an archive file to a 
-    destination directory. 
-    Wrote to provide broadest support, switches bewtween psv5 native 
-    expand-archive and .net 4.5 zipfile class support, and even oldest legacy 
-    Shell.Application Object that can work on all versions of PowerShell starting 
-    from v2 and doesn't have any dependencies on .Net Framework, but this approach 
-    can be a little slower on when enumerating and copying a large number of files. 
+    Expand-ArchiveFile.ps1 - Decompress all files in an archive file to a destination directory (wraps Psv5+ native cmdlets, and matching legacy .net calls)
+    
+    Wrote to provide broadest support, switches bewtween:
+    - PSv5+ native expand-archive 
+    - .net 4.5 zipfile class support, 
+    - and even oldest legacy Shell.Application Object that can work on all versions of PowerShell starting from v2 (and has no dependencies on .Net Framework)
+    Note:, but this approach can be a little slower on when enumerating and copying a large number of files. 
     .PARAMETER  Path
     Source archive full path [-Path c:\path-to\Path.ext]
-    .PARAMETER  Destination
-    Destination folder in which to expand all compressed files in the source archive [-Destination c:\path-to\]
+    .PARAMETER  DestinationPath
+    Destination folder in which to expand all compressed files in the source archive [-DestinationPath c:\path-to\]
     .PARAMETER  useShell
     Switch to use shell.application COM object (broadest legacy compatibility, slower for large number of files) [-useShell]
     .PARAMETER  Overwrite
@@ -36,18 +36,17 @@ function Expand-ArchiveFile {
     .OUTPUTS
     None. Returns no objects or output.
     .EXAMPLE
-    Expand-ArchiveFile -Path "C:\pathto\file.zip" -Destination "c:\pathdest\" ;
-    Expand content of specified file to destination
+    Expand-ArchiveFile -Path "C:\pathto\file.zip" -DestinationPath "c:\pathdest\" ;
+    Expand content of specified file to DestinationPath
     .EXAMPLE
-    Expand-ArchiveFile -Path "C:\pathto\file.zip" -Destination "c:\pathdest\" -useShell ;
-    Expand content of specified file to destination, fall back to legacy Shell.application support.
+    Expand-ArchiveFile -Path "C:\pathto\file.zip" -DestinationPath "c:\pathdest\" -useShell ;
+    Expand content of specified file to DestinationPath, fall back to legacy Shell.application support.
     .EXAMPLE
-    'c:\tmp\test3.zip','c:\tmp\test2.zip' | Expand-ArchiveFile -Destination "c:\pathdest\" -verbose ;
-    Pipeline example: Expand content of specified file to destination
+    'c:\tmp\test3.zip','c:\tmp\test2.zip' | Expand-ArchiveFile -DestinationPath "c:\pathdest\" -verbose ;
+    Pipeline example: Expand content of specified file to DestinationPath
     .LINK
     https://github.com/tostka/verb-XXX
     #>
-    # ($file, $destination)
     [CmdletBinding()]
     [Alias('Expand-ZipFile')]
     Param(
@@ -55,9 +54,9 @@ function Expand-ArchiveFile {
         [ValidateScript( { Test-Path $_ })]
         [Alias('File')]
         [string[]]$Path,
-        [Parameter(Mandatory = $true,Position = 1,HelpMessage = "Destination folder in which to expand all compressed files in the source archive [-Destination c:\path-to\]")]
+        [Parameter(Mandatory = $true,Position = 1,HelpMessage = "Destination folder in which to expand all compressed files in the source archive [-DestinationPath c:\path-to\]")]
         #[ValidateScript( { Test-Path $_ -PathType 'Container' })]
-        [string]$Destination,
+        [string]$DestinationPath,
         [Parameter(HelpMessage = "Overwrite switch (only used pre-psv5 when not using -useShell)[-Overwrite]")]
         [switch]$Overwrite,
         [Parameter(HelpMessage = "Switch to use shell.application COM object (broadest legacy compatibility, slower for large number of files) [-useShell]")]
@@ -83,49 +82,45 @@ function Expand-ArchiveFile {
                             $archive = $shell.NameSpace($item) ;
                         } else { 
                           write-verbose "(Using .Net class [System.IO.Compression.ZipFile]...)" ; 
-                          Add-Type -AssemblyName System.IO.Compression.FileSystem
+                          TRY{[System.IO.Compression.FileSystem]| out-null} CATCH{Add-Type -AssemblyName System.IO.Compression.FileSystem} ; 
                           $item = get-childitem -path $item ; 
                         } ; 
-                        if(-not (test-path -path $Destination)){
-                          write-host "(atempting creation of missing -destination specified folder...)" ; 
-                           New-Item -Path (split-path $destination) -Name (split-path $destination -leaf) -ItemType "directory" -whatif:$($whatif) ; 
+                        if(-not (test-path -path $DestinationPath)){
+                            write-host "(atempting creation of missing -DestinationPath specified folder...)" ; 
+                            New-Item -Path (split-path $DestinationPath) -Name (split-path $DestinationPath -leaf) -ItemType "directory" -whatif:$($whatif) ; 
                         } ; 
-                    } CATCH {
-                        $ErrTrapd=$Error[0] ;
-                        $smsg = "$('*'*5)`nFailed processing $($ErrTrapd.Exception.ItemName). `nError Message: $($ErrTrapd.Exception.Message)`nError Details: `n$(($ErrTrapd|out-string).trim())`n$('-'*5)" ;
-                        if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level WARN } #Error|Warn|Debug 
-                        else{ write-warning "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ;
-                        Break ; 
-                    } ;   
+                    }CATCH{
+                        Write-Warning -Message $_.Exception.Message; 
+                        BREAK ; 
+                    } ; 
                     if($useShell){
                         foreach ($item in $archive.items()) {
                             TRY{  
-                                $shell.Namespace($destination).copyhere($item) ;
+                                $shell.Namespace($DestinationPath).copyhere($item) ;
                             }CATCH{
                                 Write-Warning -Message $_.Exception.Message ; 
-                                continue ; 
+                                CONTINUE ; 
                             } ;     
                         } ;  # loop-E
                     } else { 
                         TRY{
                             $entries = [IO.Compression.ZipFile]::OpenRead($item.FullName).Entries ; 
                             $entries | ForEach-Object -Process {
-                                [IO.Compression.ZipFileExtensions]::ExtractToFile($_,"$($Destination)\$($_)",$Overwrite) ; 
+                                [IO.Compression.ZipFileExtensions]::ExtractToFile($_,"$($DestinationPath)\$($_)",$Overwrite) ; 
                             } ; 
                         }CATCH{
                             Write-Warning -Message $_.Exception.Message ; 
                         } ; 
                     } ; 
                 } else { 
-                    # psv5+: use native Expand-Archive
+                    write-verbose "(PSv5+: using native Expand-Archive)" ; 
                     $pltEA=[ordered]@{
-                        Destination = $destination ; 
+                        DestinationPath = $DestinationPath ; 
                         erroraction = 'STOP' ;
                         #whatif = $($whatif) ;
                     } ;
                     if ($item -match "(\[|\])") {
                         write-verbose "(angle bracket chars detected in -Path:switching to -LiteralPath support)" ; 
-                        #$pltEA.remove('Path') ; 
                         $pltEA.Add('LiteralPath',$item) ; 
                     } else {
                         $pltEA.Add('Path',$item) ; 
@@ -139,6 +134,6 @@ function Expand-ArchiveFile {
                     } ; 
                 } ;  # if-E psv5
         } ;  # loop-E
-    }  # if-E PROC
+    }  # E PROC
 } ; 
 #*------^ END Function  ^------
