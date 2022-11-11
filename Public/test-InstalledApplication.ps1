86207dd7456b26549f85c78e31145c1572f04e72
Tostka <tostka@users.noreply.github.com>
2022-11-11 11:13:39 -0600

    test-InstalledApplication
    * 10:37 AM 11/11/2022 ren get-InstalledApplication -> test-InstalledApplication (better match for function, default is test -detailed triggers dump back); aliased orig name; also pulling in overlapping verb-desktop:check-ProgramInstalled(), aliased -Name with ported programNam ; CBH added expl output demo

--- Public/test-InstalledApplication.ps1
+++ Public/test-InstalledApplication.ps1
@@ -1,51 +1,59 @@
-﻿#*------v Function get-InstalledApplication v------
-Function get-InstalledApplication {
+﻿#*------v Function test-InstalledApplication v------
+Function test-InstalledApplication {
     <#
     .SYNOPSIS
-    get-InstalledApplication.ps1 - Check registry for Installed status of specified application (checks x86 & x64 Uninstall hives, for substring matches on Name)
+    test-InstalledApplication.ps1 - Check registry for Installed status of specified application (checks x86 & x64 Uninstall hives, for substring matches on Name)
     .NOTES
     Version     : 1.0.0
     Author      : Todd Kadrie
     Website     : http://www.toddomation.com
     Twitter     : @tostka / http://twitter.com/tostka
     CreatedDate : 20210415-0913AM
-    FileName    : get-InstalledApplication
+    FileName    : test-InstalledApplication
     License     : MIT License
     Copyright   : (c) 2020 Todd Kadrie
     Github      : https://github.com/tostka/verb-XXX
     Tags        : Powershell,Application,Install
     REVISIONS
+    * 10:37 AM 11/11/2022 ren get-InstalledApplication -> test-InstalledApplication (better match for function, default is test -detailed triggers dump back); aliased orig name; also pulling in overlapping verb-desktop:check-ProgramInstalled(), aliased -Name with ported programNam ; CBH added expl output demo
     * 9:13 AM 4/15/2021 init vers
     .DESCRIPTION
-    get-InstalledApplication.ps1 - Check registry for Installed status of specified application (checks x86 & x64 Uninstall hives, for substring matches on Name)
+    test-InstalledApplication.ps1 - Check registry for Installed status of specified application (checks x86 & x64 Uninstall hives, for substring matches on Name)
     .INPUTS
     None. Does not accepted piped input.
     .OUTPUTS
     Returns either System.Boolean (default) or System.Object (-detail)
     .EXAMPLE
-    if(get-InstalledApplication -name "powershell"){"yes"} else { "no"} ; 
+    PS> if(test-InstalledApplication -name "powershell"){"yes"} else { "no"} ; 
+    yes
     Default boolean test
     .EXAMPLE    
-    get-InstalledApplication -name "powershell" -detail -verbose; 
+    PS> get-InstalledApplication -Name 'google drive' -detail
+    DisplayName  DisplayVersion InstallLocation                                                      Publisher
+    -----------  -------------- ---------------                                                      ---------
+    Google Drive 63.0.5.0       C:\Program Files\Google\Drive File Stream\63.0.5.0\GoogleDriveFS.exe Google LLC
     Example returning detail (DisplayName and InstallLocation)
     .LINK
-    https://github.com/tostka/verb-ex2010
-    .LINK
+    https://github.com/tostka/verb-io
     #>
     [CmdletBinding()]
+    [Alias('check-ProgramInstalled','get-InstalledApplication')]
     PARAM(
         [Parameter(Position=0,HelpMessage="Application Name substring[-Name Powershell]")]
+        [Alias('programNam')]
         $Name,
         [Parameter(HelpMessage="Debugging Flag [-Return detailed object on match]")]
         [switch] $Detail
     ) ;
     $x86Hive = Get-ChildItem 'HKLM:Software\Microsoft\Windows\CurrentVersion\Uninstall' |
          % { Get-ItemProperty $_.PsPath } | ?{$_.displayname -like "*$($Name)*"} ;
+    write-verbose "`$x86Hive:$([boolean]$x86Hive)" ; 
     if(Test-Path 'HKLM:Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall'){
         #$x64Hive = ((Get-ChildItem "HKLM:Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall") |
         #    Where-Object { $_.'Name' -like "*$($Name)*" } ).Length -gt 0;
         $x64Hive = Get-ChildItem 'HKLM:Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall' |
             % { Get-ItemProperty $_.PsPath } | ?{$_.displayname -like "*$($Name)*"} ;
+         write-verbose "`$x64Hive:$([boolean]$x64Hive)" ; 
     }
     if(!$Detail){
         # boolean return:
@@ -55,4 +63,4 @@ Function get-InstalledApplication {
         $x86Hive | Select $props | write-output ; 
         $x64Hive | Select $props | write-output ; 
     } ; 
-} #*------^ END Function get-InstalledApplication ^------
+} #*------^ END Function test-InstalledApplication ^------
