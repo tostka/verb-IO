edde283072705bc64e6a47e830a01c04fcd9597b
Tostka <tostka@users.noreply.github.com>
2022-11-30 12:33:30 -0600

    20221130-1233PM:EOD closing commit

--- Public/stop-driveburn.ps1
+++ Public/stop-driveburn.ps1
@@ -15,6 +15,7 @@ Function stop-driveburn {
     Github      : https://github.com/tostka/verb-XXX
     Tags        : Powershell,Performance,Workstation
     REVISIONS
+    * 10:50 AM 11/29/2022 added logging, to track how often landesk processes are impeding productive work.
     7:35 AM 10/5/2020 ported to verb-IO, updated tsksid/admin-incl-ServerCore.ps1
     * 8:48 AM 10/22/2019 added ldesk gatherproducts
     *11:34 AM 4/16/2019 rearranging cmd order
@@ -41,22 +42,108 @@ Function stop-driveburn {
     [CmdletBinding()]
     [Alias('sdb')]
     Param([Parameter(HelpMessage="Whatif Flag  [-whatIf]")][switch] $whatIf) ; 
-    write-host -foregroundcolor green "$((get-date).ToString('HH:mm:ss')):KILLING DRIVE SUCKERS!`nQUIT READING BY THE HARDDRIVE LIGHT!" ; 
+    
+    #*======v SUB MAIN v======
+    # add logging support
+    #region INIT; # ------
+    # Get the name of this function
+    ${CmdletName} = $PSCmdlet.MyInvocation.MyCommand.Name ;
+    # Get parameters this function was invoked with
+    $PSParameters = New-Object -TypeName PSObject -Property $PSBoundParameters ;
+    $verbose = ($VerbosePreference -eq "Continue") ; 
+    $NoLoop = $true ; 
+    # SETUP CROSS-VERSION-COMPAT. $PS* VARIABLES
+    if($showDebug){write-debug -verbose:$true "`SHOWDEBUG: `$PSScriptRoot:$($PSScriptRoot)`n`$PSCommandPath:$($PSCommandPath)"} ;
+    if ($PSScriptRoot -eq "") {
+        if ($psISE){
+            $ScriptName = $psISE.CurrentFile.FullPath ;
+        } elseif ($context = $psEditor.GetEditorContext()) {
+            $ScriptName = $context.CurrentFile.Path ;
+        } elseif($host.version.major -lt 3){
+            $ScriptName = $MyInvocation.MyCommand.Path ;
+            $PSScriptRoot = Split-Path $ScriptName -Parent ;
+            $PSCommandPath = $ScriptName ;
+        } else {
+            if($MyInvocation.MyCommand.Path) {
+                $ScriptName = $MyInvocation.MyCommand.Path ;
+                $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent ;
+            } else {
+                throw "UNABLE TO POPULATE SCRIPT PATH, EVEN `$MyInvocation IS BLANK!" ;
+            } ;
+        };
+        $ScriptDir = Split-Path -Parent $ScriptName ;
+        $ScriptBaseName = split-path -leaf $ScriptName ;
+        $ScriptNameNoExt = [system.io.path]::GetFilenameWithoutExtension($ScriptName) ;
+    } else {
+        $ScriptDir = $PSScriptRoot ;
+        if($PSCommandPath){
+            $ScriptName = $PSCommandPath ;
+        } else {
+            $ScriptName = $myInvocation.ScriptName
+            $PSCommandPath = $ScriptName ;
+        } ;
+        $ScriptBaseName = (Split-Path -Leaf ((&{$myInvocation}).ScriptName))  ;
+        $ScriptNameNoExt = [system.io.path]::GetFilenameWithoutExtension($MyInvocation.InvocationName) ;
+    } ; 
+    if($showDebug){write-debug -verbose:$true "`$ScriptDir:$($ScriptDir)`n`$ScriptBaseName:$($ScriptBaseName)`n`$ScriptNameNoExt:$($ScriptNameNoExt)`n`$PSScriptRoot:$($PSScriptRoot)`n`$PSCommandPath:$($PSCommandPath)" ; } ;
+    #-=-=-=-=-=-=-=-=
+
+    $ParentPath = $MyInvocation.MyCommand.Definition ; 
+    if($ParentPath){
+        $rgxProfilePaths='(\\Documents\\WindowsPowerShell\\scripts|\\Program\sFiles\\windowspowershell\\scripts)' ; 
+        if($ParentPath -match $rgxProfilePaths){
+            $ParentPath = "$(join-path -path 'c:\scripts\' -ChildPath (split-path $ParentPath -leaf))" ; 
+        } ; 
+        if($NoLoop){
+            $logspec = start-Log -Path ($ParentPath) -showdebug:$($showdebug) -whatif:$($whatif) -NoTimestamp ;
+            $smsg = "-NoLoop specified:`$logspec returned:$(($logspec|out-string).trim())" ; 
+            if($verbose){ if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level Info } #Error|Warn|Debug 
+            else{ write-host -foregroundcolor green "$((get-date).ToString('yyyyMMdd-HHmm')):$($smsg)" } ; } ; 
+        } else { 
+            $logspec = start-Log -Path ($ParentPath) -showdebug:$($showdebug) -whatif:$($whatif) ;
+            $smsg = "(Looping...):`$logspec returned:$(($logspec|out-string).trim())" ; 
+            if($verbose){ if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level Info } #Error|Warn|Debug 
+            else{ write-host -foregroundcolor green "$((get-date).ToString('yyyyMMdd-HHmm')):$($smsg)" } ; } ; 
+
+        } ; 
+        if($logspec){
+            $logging=$logspec.logging ;
+            $logfile=$logspec.logfile ;
+            $transcript=$logspec.transcript ;
+        } else {$smsg = "Unable to configure logging!" ; write-warning "$((get-date).ToString('yyyyMMdd-HHmm')):$($sMsg)" ; Exit ;} ;
+    } else {$smsg = "No functional `$ParentPath found!" ; write-warning "$((get-date).ToString('yyyyMMdd-HHmm')):$($sMsg)" ;  Exit ;} ;
+
+    $smtpFrom = (($scriptBaseName.replace(".","-")) + "@toro.com") ; 
+
+    #endregion INIT; # ------
+    
+    $smsg = "$((get-date).ToString('HH:mm:ss')):KILLING DRIVE SUCKERS!`nQUIT READING BY THE HARDDRIVE LIGHT!" ; 
+    if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level Info } 
+    else{ write-host -foregroundcolor green "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ;
+    #Levels:Error|Warn|Info|H1|H2|H3|Debug|Verbose|Prompt
     "LDIScn32","Cagent32","gatherproducts" | foreach {
         #"==Checking for $($_)" ;
         if($gp=get-process "$($_)" -ea 0 ){
-            write-host -foregroundcolor green "$((get-date).ToString('HH:mm:ss')):PROCMATCH:Stopping proc:`n$(($gp | ft -auto ID,ProcessName|out-string).trim())" ; 
+            $smsg = "$((get-date).ToString('HH:mm:ss')):PROCMATCH:Stopping proc:`n$(($gp | ft -auto ID,ProcessName|out-string).trim())" ; 
+            if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level Info } 
+            else{ write-host -foregroundcolor green "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ;
             $gp | stop-process -force -whatif:$($whatif)
         } else {
-            write-host -foregroundcolor green "$((get-date).ToString('HH:mm:ss')):(no $($_) processes found)" ; 
+            $smsg = "$((get-date).ToString('HH:mm:ss')):(no $($_) processes found)" ; 
+            if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level Info } 
+            else{ write-host -foregroundcolor green "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ;
         } ; 
     } ; 
 
     #  tack index stop in here too
     $stat=(Get-Service -Name wsearch).status ; 
-    write-host -foregroundcolor green "$((get-date).ToString('HH:mm:ss')):===Windows Search:Status:$($stat)" ; 
+    $smsg = "$((get-date).ToString('HH:mm:ss')):===Windows Search:Status:$($stat)" ; 
+    if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level Info } 
+    else{ write-host -foregroundcolor green "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ;
     if ($stat -eq 'Running') {
-        write-host -foregroundcolor green "$((get-date).ToString('HH:mm:ss')):STOPPING WSEARCH SVC" ; 
+        $smsg = "$((get-date).ToString('HH:mm:ss')):STOPPING WSEARCH SVC" ; 
+        if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level Info } 
+        else{ write-host -foregroundcolor green "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ;
         stop-Service -Name wsearch ; 
     } else { 
         write-verbose "$((get-date).ToString('HH:mm:ss')):(wsearch is *not* running)" ; 
