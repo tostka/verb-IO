70ff94cf43b4fa4a5c6086eb7f5bc6ebd98f8de9
Tostka <tostka@users.noreply.github.com>
2023-02-02 15:03:35 -0600

    Update write-HostIndent.ps1
    * 3:02 PM 2/2/2023 rolled back overwrite with w-l() code ; updated CBH

--- Public/write-HostIndent.ps1
+++ Public/write-HostIndent.ps1
@@ -1,15 +1,16 @@
-﻿#*------v Function set-HostIndent v------
-function set-HostIndent {
+﻿
+#*------v Function write-HostIndent v------
+function write-HostIndent {
     <#
     .SYNOPSIS
-    set-HostIndent - Utility cmdlet that explicitly forces the $env:HostIndentSpaces to a known interval of 4 (or the configured `$PadIncrement)
+    write-HostIndent - write-host wrapper that adds a stock $env:HostIndentSpaces to the left of each line of text sent (splits lines prior to indenting
     .NOTES
     Version     : 0.0.5
     Author      : Todd Kadrie
     Website     : http://www.toddomation.com
     Twitter     : @tostka / http://twitter.com/tostka
     CreatedDate : 2023-01-12
-    FileName    : set-HostIndent.ps1
+    FileName    : write-HostIndent.ps1
     License     : MIT License
     Copyright   : (c) 2022 Todd Kadrie
     Github      : https://github.com/tostka/verb-io
@@ -18,15 +19,17 @@ function set-HostIndent {
     AddedWebsite: https://community.spiceworks.com/people/lburlingame
     AddedTwitter: URL
     REVISIONS
+    * 3:02 PM 2/2/2023 rolled back overwrite with w-l() code ; updated CBH
     * 2:01 PM 2/1/2023 add: -PID param
-    * 2:39 PM 1/31/2023 updated to work with $env:HostIndentSpaces in process scope. 
+    * 2:39 PM 1/31/2023 updated to work with $env:HostIndentSpaces in process scope.
     * 9:50 AM 1/17/2023 All need to be recoded to use evari's, scoped varis aren't consistently discoverable, to have the current 'indent depth' being tweaked by pop|push|reset|set and read by write:
-    * 4:39 PM 1/11/2023 init
+    * 11:50 AM 1/11/2023 ren $indentNum -> $HostIndentSpaces -> $env:HostIndentSpaces
+    * 4:15 PM 1/10/2023 ren printIndent -> write-HostIndent ; expanded CBH example, Heart of this is in the example, not the simple write-host loop; 
+        added to verb-io.
+    * 2:06 PM 1/9/2023 add: CBH, $indentname driving this is is a parent funciton variable, tweak it on the fly to move cursor left or right.
     .DESCRIPTION
-    set-HostIndent - Utility cmdlet that explicitly forces the $env:HostIndentSpaces to a known interval of 4 (or the configured `$PadIncrement)
 
-    If it doesn't find a preexisting $IndentHostSpaces in 'private','local','script','global'
-    it throws an error
+    write-HostIndent - write-host wrapper that adds a stock $env:HostIndentSpaces to the left of each line of text sent (splits lines prior to indenting
 
     Part of the verb-HostIndent set:
     write-HostIndent # write-host wrapper that indents/pads each line of output a fixed amount (driven by $env:HostIndentSpaces common variable). 
@@ -35,55 +38,126 @@ function set-HostIndent {
     pop-HostIndent # $env:HostIndentSpaces = ([int]($env:HostIndentSpaces) - 4) ;
     set-HostIndent # explicit set to multiples of 4
             
-
-    Concept inspired by L5257's printIndent() in his get-DNSspf.ps1 (which ran a simple write-host -nonewline loop, to be run prior to write-host use). 
-
-    .PARAMETER PadIncrement
-    Number of spaces to pad by default (defaults to 4).[-PadIncrement 8]
+    .PARAMETER BackgroundColor
+    Specifies the background color. There is no default. The acceptable values for this parameter are:
+    (Black | DarkBlue | DarkGreen | DarkCyan | DarkRed | DarkMagenta | DarkYellow | Gray | DarkGray | Blue | Green | Cyan | Red | Magenta | Yellow | White)
+    .PARAMETER ForegroundColor <System.ConsoleColor>
+    Specifies the text color. There is no default. The acceptable values for this parameter are:
+    (Black | DarkBlue | DarkGreen | DarkCyan | DarkRed | DarkMagenta | DarkYellow | Gray | DarkGray | Blue | Green | Cyan | Red | Magenta | Yellow | White)
+    .PARAMETER NoNewline <System.Management.Automation.SwitchParameter>
+    The string representations of the input objects are concatenated to form the output. No spaces or newlines are inserted between
+    the output strings. No newline is added after the last output string.
+    .PARAMETER Object <System.Object>
+    Objects to display in the host.
+    .PARAMETER Separator <System.Object>
+    Specifies a separator string to insert between objects displayed by the host.
+    .PARAMETER PadChar
+    Character to use for padding (defaults to a space).[-PadChar '-']
     .PARAMETER usePID
     Switch to use the `$PID in the `$env:HostIndentSpaces name (Env:HostIndentSpaces`$PID)[-usePID]
     .EXAMPLE
-    PS> set-HostIndent ;
-    PS> $env:HostIndentSpaces ; 
+    PS> $env:HostIndentSpaces = 4 ; 
+    PS> write-HostIndent 'indented message'
     Simple indented text demo
+    .EXAMPLE
+    PS>  write-verbose 'set to baseline' ; 
+    PS>  reset-HostIndent ; 
+    PS>  write-host "`$env:HostIndentSpaces:$($env:HostIndentSpaces)" ; 
+    PS>  write-verbose 'write an H1 banner'
+    PS>  $sBnr="#*======v  H1 Banner: v======" ;
+    PS>  $smsg = $sBnr ;
+    PS>  Write-HostIndent -ForegroundColor yellow $smsg ;
+    PS>  write-verbose 'push indent level+1' ; 
+    PS>  push-HostIndent ; 
+    PS>  write-host "`$env:HostIndentSpaces:$($env:HostIndentSpaces)" ; 
+    PS>  $smsg = "This is information (indented)" ; 
+    PS>  Write-HostIndent -ForegroundColor Gray $smsg ;
+    PS>  write-verbose 'push indent level+2' ; 
+    PS>  push-HostIndent ; 
+    PS>  write-host "`$env:HostIndentSpaces:$($env:HostIndentSpaces)" ; 
+    PS>  write-verbose 'write a PROMPT entry with -Indent specified' ; 
+    PS>  $smsg = "This is a subset of information (indented)" ; 
+    PS>  Write-HostIndent -ForegroundColor Gray $smsg ;
+    PS>  write-verbose 'pop indent level out one -1' ; 
+    PS>  pop-HostIndent ; 
+    PS>  write-verbose 'write a Success entry with -Indent specified' ; 
+    PS>  $smsg = "This is a Successful information (indented)" ; 
+    PS>  PS>  Write-HostIndent -ForegroundColor green $smsg ; ;
+    PS>  write-verbose 'reset to baseline for trailing banner'
+    PS>  reset-HostIndent ; 
+    PS>  write-host "`$env:HostIndentSpaces:$($env:HostIndentSpaces)" ; 
+    PS>  write-verbose 'write the trailing H1 banner'
+    PS>  $smsg = "$($sBnr.replace('=v','=^').replace('v=','^='))" ;
+    PS>  Write-HostIndent -ForegroundColor yellow $smsg ;
+    PS>  write-verbose 'clear indent `$env:HostIndentSpaces' ; 
+    PS>  clear-HostIndent ; 
+    PS>  write-host "`$env:HostIndentSpaces:$($env:HostIndentSpaces)" ; 
+        
+        $env:HostIndentSpaces:0
+        16:16:17: #  #*======v  H1 Banner: v======
+        $env:HostIndentSpaces:4
+            16:16:17: INFO:  This is information (indented)
+        $env:HostIndentSpaces:8
+                16:16:17: PROMPT:  This is a subset of information (indented)
+            16:16:17: SUCCESS:  This is a Successful information (indented)
+        $env:HostIndentSpaces:0
+        16:16:17: #  #*======^  H1 Banner: ^======
+        $env:HostIndentSpaces:
+
+    Demo broad process for use of verb-HostIndent funcs and write-log with -indent parameter.
     .EXAMPLE
     PS>  $domain = 'somedomain.com' ; 
     PS>  write-verbose "Top of code, establish 0 Indent" ;
     PS>  $env:HostIndentSpaces = 0 ;
     PS>  #...
     PS>  # indent & do nested thing
-    PS>  $env:HostIndentSpaces = ([int]($env:HostIndentSpaces) + 4) ; ;
+    PS>  $env:HostIndentSpaces = ([int]($env:HostIndentSpaces) + 4) ;
     PS>  write-verbose "`$env:HostIndentSpaces: -= 4: Net:$($env:HostIndentSpaces)" ;
-    PS>  set-HostIndent "Spf Version: $($spfElement)" -verbose ;
+    PS>  Write-HostIndent "Spf Version: $($spfElement)" -verbose ;
     PS>  # nested another level
-    PS>  $env:HostIndentSpaces = ([int]($env:HostIndentSpaces) + 4) ; ;
+    PS>  $env:HostIndentSpaces = ([int]($env:HostIndentSpaces) + 4) ;
     PS>  write-verbose "`$env:HostIndentSpaces: += 4 Net:$($env:HostIndentSpaces)" ;
-    PS>  set-HostIndent -ForegroundColor Gray "($Domain)" -verbose ;
+    PS>  Write-HostIndent -ForegroundColor Gray "($Domain)" -verbose ;
     PS>  # unindent back out
-    PS>  $env:HostIndentSpaces = ([int]($env:HostIndentSpaces) - 4) ; ;
+    PS>  $env:HostIndentSpaces = ([int]($env:HostIndentSpaces) - 4) ;
     PS>  write-verbose "`$env:HostIndentSpaces: -= 4: Net:$($env:HostIndentSpaces)" ;
     PS>  # unindent back out
-    PS>  $env:HostIndentSpaces = ([int]($env:HostIndentSpaces) - 4) ; ;
+    PS>  $env:HostIndentSpaces = ([int]($env:HostIndentSpaces) - 4) ;
     PS>  write-verbose "`$env:HostIndentSpaces: -= 4: Net:$($env:HostIndentSpaces)" ;
-    PS>  set-HostIndent -ForegroundColor Gray "($Domain)" -verbose ;
-    SAMPLEOUTPUT
-    DESCRIPTION
-    SAMPLEOUTPUT
-    DESCRIPTION        
+    PS>  Write-HostIndent -ForegroundColor Gray "($Domain)" -verbose ;
+                Spf Version:
+                    (somedomain.com)
+            (somedomain.com)            
+    Demo push/popping $env:HostIndentSpaces and using write-hostIndent
     #>
     [CmdletBinding()]
-    [Alias('pop-hi')]
+    [Alias('w-hi')]
+            
     PARAM(
-        [Parameter(Position=0,
-            HelpMessage="Number of spaces to set write-hostIndent current indent (`$scop:HostIndentpaces) to.[-Spaces 8]")]
-            [int]$Spaces,
         [Parameter(
-            HelpMessage="Number of spaces to pad by default (defaults to 4).[-PadIncrement 8]")]
-        [int]$PadIncrement = 4,
+            HelpMessage="Specifies the background color. There is no default. The acceptable values for this parameter are:
+    (Black | DarkBlue | DarkGreen | DarkCyan | DarkRed | DarkMagenta | DarkYellow | Gray | DarkGray | Blue | Green | Cyan | Red | Magenta | Yellow | White)")]
+            [System.ConsoleColor]$BackgroundColor,
         [Parameter(
-            HelpMessage="Mathematical rounding logic to use for calculating nearest multiple of PadIncrement (RoundUp|RoundDown|AwayFromZero|Midpoint, default:RoundUp)[-Rounding awayfromzero]")]
-            [ValidateSet('RoundUp','RoundDown','AwayFromZero','Midpoint')]
-            [string]$Rounding = 'RoundUp',
+            HelpMessage="Specifies the text color. There is no default. The acceptable values for this parameter are:
+(Black | DarkBlue | DarkGreen | DarkCyan | DarkRed | DarkMagenta | DarkYellow | Gray | DarkGray | Blue | Green | Cyan | Red | Magenta | Yellow | White)")]
+            [System.ConsoleColor]$ForegroundColor,
+        [Parameter(
+            HelpMessage="The string representations of the input objects are concatenated to form the output. No spaces or newlines are inserted between
+the output strings. No newline is added after the last output string.")]
+            [System.Management.Automation.SwitchParameter]$NoNewline,
+        [Parameter(Position=0,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true,
+            HelpMessage="Objects to display in the host")]
+            [System.Object]$Object,
+        [Parameter(
+            HelpMessage="Specifies a separator string to insert between objects displayed by the host.")]
+            [System.Object]$Separator,
+        [Parameter(
+            HelpMessage="Character to use for padding (defaults to a space).[-PadChar '-']")]
+            [string]$PadChar = ' ',
+        [Parameter(
+            HelpMessage="Number of spaces to pad by default (defaults to 4).[-PadIncrment 8]")]
+        [int]$PadIncrment = 4,
         [Parameter(
             HelpMessage="Switch to use the `$PID in the `$env:HostIndentSpaces name (Env:HostIndentSpaces`$PID)[-usePID]")]
             [switch]$usePID
@@ -98,30 +172,21 @@ function set-HostIndent {
         #$VerbosePreference = "SilentlyContinue" ;
         #endregion CONSTANTS-AND-ENVIRO #*======^ END CONSTANTS-AND-ENVIRO ^======
 
-        write-verbose "$($CmdletName): Using `$PadIncrement:`'$($PadIncrement)`'" ; 
-
-        switch($Rounding){
-            'RoundUp' {
-                # always round up (to next higher multiple)
-                $Spaces = ([system.math]::ceiling($Spaces/$PadIncrement))*$PadIncrement  ; 
-                write-verbose "Rounding:Roundup specified: Rounding to: $($Spaces)" ; 
-                }
-            'RoundDown' {
-                # always round down (to next lower multiple)
-                $Spaces = ([system.math]::floor($Spaces/$PadIncrement))*$PadIncrement  ; 
-                write-verbose "Rounding:RoundDown specified: Rounding to: $($Spaces)" ;
-                }
-            'AwayFromZero' {
-                # traditional school: 'when remainder is 5 round up'
-                $Spaces = ([system.math]::round($_/$PadIncrement,0,1))*$PadIncrement  ;
-                write-verbose "Rounding:AwayFromZero specified: Rounding to: $($Spaces)" ;
-            }
-            'Midpoint' {
-                # default programatic/banker's rounding: if midpoint 5, round to the *nearest even number*'
-                $Spaces = ([system.math]::round($_/$PadIncrement))*$PadIncrement  ;
-                write-verbose "Rounding:Midpoint specified: Rounding to: $($Spaces)" ;
-            }
+        $pltWH = @{} ; 
+        if ($PSBoundParameters.ContainsKey('BackgroundColor')) {
+            $pltWH.add('BackgroundColor',$BackgroundColor) ; 
         } ;
+        if ($PSBoundParameters.ContainsKey('ForegroundColor')) {
+            $pltWH.add('ForegroundColor',$ForegroundColor) ; 
+        } ;
+        if ($PSBoundParameters.ContainsKey('NoNewline')) {
+            $pltWH.add('NoNewline',$NoNewline) ; 
+        } ;
+        if ($PSBoundParameters.ContainsKey('Separator')) {
+            $pltWH.add('Separator',$Separator) ; 
+        } ;
+
+        write-verbose "$($CmdletName): Using `$PadChar:`'$($PadChar)`'" ; 
 
         #if we want to tune this to a $PID-specific variant, could use:
         if($usePID){
@@ -138,23 +203,62 @@ function set-HostIndent {
         if (-not ([int]$CurrIndent = (Get-Item -Path $HISName -erroraction SilentlyContinue).Value ) ){
             [int]$CurrIndent = 0 ; 
         } ; 
-                
+
         write-verbose "$($CmdletName): Discovered `$$($HISName):$($CurrIndent)" ; 
-        $pltSV=[ordered]@{
-            Path = $HISName ;  
-            Value = $Spaces; 
-            Force = $true ; 
-            erroraction = 'STOP' ;
-        } ;
-        $smsg = "$($CmdletName): Set 1 lvl:Set-Variable w`n$(($pltSV|out-string).trim())" ; 
-        write-verbose $smsg  ;
-        TRY{
-            Set-Item @pltSV #-verbose ; 
-        } CATCH {
-            $smsg = $_.Exception.Message ;
-            write-WARNING "$((get-date).ToString('HH:mm:ss')):$($smsg)" ;
-            BREAK ;
-        } ;
+
+        <# some methods to left pad console output: Most add padding within the obj written by write- host: youend up with a big block of color, if you use fore/back w w-h:
+
+            [Indentation the Write-host output in Powershell - Stack Overflow - stackoverflow.com/](https://stackoverflow.com/questions/70895830/indentation-the-write-host-output-in-powershell)
+                    
+            $padding = "       "
+            'test', 'test', 'test' | ForEach-Object { Write-Host ${padding}$_ }
+
+            $padding = ' ' * 20; $padding + ('test', 'test', 'test' -join "`r`n$padding")
+
+            'test', 'test', 'test' | ForEach-Object { $_.PadLeft(20) }
+
+            'test', 'test', 'test' | ForEach-Object { '{0,10}' -f $_ }
+            # OR
+            'test', 'test', 'test' | ForEach-Object { [string]::Format('{0,10}', $_) }
+
+            'test', 'test', 'test' | ForEach-Object { $_ -replace '^', (' ' * 20) }
+
+            'test', 'test', 'test' -replace '(?m)^(.{0,20})',{" " * (20 - $_.Length) + $_.Groups[0].Value}
+
+            Trick is, you want to use non-color w-h -nonewline, the number of indent spaces you need
+            And then you write-host with colors etc specd.
+        #>
+
+        # if $object has multiple lines, split it:
+        $Object = $Object.Split([Environment]::NewLine)
+                
+        <# Issue with most above: if you use:
+        $padding = "-" * 4 ; # to *see* the spaces
+        $whdgy = @{ 'BackgroundColor' = 'Yellow' 'ForegroundColor' = 'DarkGreen' }; 
+        $smsg.Split([Environment]::NewLine) | %{write-host -obj ${padding}$_ @whdgy}
+        ```
+        Output:
+        ----Fail on prior TXT qry
+        ----Retrying TXT qry:-Name .
+        ----Resolve-DnsName -Type TXT -Name 
+            you get highlights/color the *entire length of the line*, including the whitespace, padchars
+            #>
+
+        # you need to do the w-h -nonewline separately, before using w-h per line of object, with colors:
+        <# works, even recog's muiltiline object outputs and splits them at the lines, indenting them cleanly.
+        foreach ($obj in $object){
+            for ($n=0; $n -lt [int]$env:HostIndentSpaces; $n++) {
+		        Write-Host -NoNewline $PadChar ; 
+	        } ; 
+            write-host @pltWH -object $obj ; 
+        } ; 
+        #>
+        # works, is equiv to the above, just collapses down the silly -nonewline loop
+        foreach ($obj in $object){
+            Write-Host -NoNewline $($PadChar * $CurrIndent)  ; 
+            write-host @pltWH -object $obj ; 
+        } ; 
+
     } ;  # BEG-E
 } ; 
-#*------^ END Function set-HostIndent ^------
+#*------^ END Function write-HostIndent ^------
