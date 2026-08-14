e76b037ac7e5fe19851a9abe191cb9e766c808db
Tostka <tostka@users.noreply.github.com>
2026-08-14 12:24:56 -0500

    Public\repair-FileEncoding.ps1
    repair-FileEncodingTDO.ps1 - Filter specified Path for risky high-ascii chars & Encoding conversion failure markers, replace each with a low-ascii equiv, (or dash for diamond questionmarks), and convert output file to UTF8

--- Public/repair-FileEncoding.ps1
+++ Public/repair-FileEncoding.ps1
@@ -1,10 +1,10 @@
-﻿# repair-FileEncoding.ps1
+﻿# repair-FileEncodingTDO.ps1
 
-#*------v Function repair-FileEncoding v------
-function repair-FileEncoding {
+#region REPAIR_FILEENCODINGTDO, ; #*------v repair-FileEncodingTDO v------
+function repair-FileEncodingTDO {
     <#
     .SYNOPSIS
-    repair-FileEncoding.ps1 - Filter specified Path for risky high-ascii chars & Encoding conversion failure markers, replace each with a low-ascii equiv, (or dash for diamond questionmarks), and convert output file to UTF8
+    repair-FileEncodingTDO.ps1 - Filter specified Path for risky high-ascii chars & Encoding conversion failure markers, replace each with a low-ascii equiv, (or dash for diamond questionmarks), and convert output file to UTF8
     .NOTES
     Version     : 1.6.2
     Author      : Todd Kadrie
@@ -19,6 +19,8 @@ function repair-FileEncoding {
     AddedWebsite:	URL
     AddedTwitter:	URL
     REVISIONS
+    * 7:48 AM 8/6/2026 flipped default C:\sc\powershell\ Path to interactive prompt: prompt for input path, or use default path. 
+    Also added alias repair-BinaryFileTDO (targeted at githubd's failure to properly parse out files with encoding shifts); ren repair-FileEncoding -> repair-FileEncodingTDO, add alias for old name
     * 10:11 AM 12/8/2022 pull *all* reqs; verb-logging is cross broken too; nested-limit triggering recursive req's verb-io
     * 4:37 PM 11/22/2022 moved into verb-io; fixed typo utf8 spec (utf-8), flip into an adv function;refactored, added broader try/catch, handling for both 
     file or dir path; simplified logic, does encoding repair always, always detects 
@@ -30,7 +32,7 @@ function repair-FileEncoding {
     * 3:00 PM 1/2/2020 got through live pass using ConvertToUTF8Only, initial review of chgs in ghd appears to be functional
     * 2:52 PM 1/2/2020 used a prior version of the replc spec successfully, now have written in the broad conversion, and debugged, but not run prod yet.
     .DESCRIPTION
-    repair-FileEncoding.ps1 - Filter specified Path for risky high-ascii chars & Encoding conversion failure markers, replace each with a low-ascii equiv, (or dash for diamond questionmarks), and convert output file to UTF8. Processess all matching ps1, psm1 & psd1 files in the tree.
+    repair-FileEncodingTDO.ps1 - Filter specified Path for risky high-ascii chars & Encoding conversion failure markers, replace each with a low-ascii equiv, (or dash for diamond questionmarks), and convert output file to UTF8. Processess all matching ps1, psm1 & psd1 files in the tree.
     doesn't like it when you copy in a revised file from another box from debugging, and the encoding has changed: Tends to describe the file as 'This binary file has changed'. 
     Fix requires flipping the encoding back. 
     Underlying GIT issue documented here:
@@ -65,40 +67,72 @@ function repair-FileEncoding {
     .PARAMETER Whatif
     Parameter to run a Test no-change pass [-Whatif switch]
     .EXAMPLE
-    PS> repair-FileEncoding.ps1 -replaceChars
+    PS> repair-FileEncodingTDO.ps1 -replaceChars
     In files in default path (C:\sc\powershell), in files Where-Object high-ascii chars are found, replace the chars with matching low-bit chars (whatif is autoforced true to ensure no accidental runs)
     .EXAMPLE
-    PS> repair-FileEncoding.ps1 -path C:\sc\verb-AAD -replacechars -whatif:$false ;
+    PS> repair-FileEncodingTDO.ps1 -path C:\sc\verb-AAD -replacechars -whatif:$false ;
     Exec-pass: problem char files, replacements, with explicit path and overridden whatif
     .EXAMPLE
     PS> gci c:\sc\ -recur| ?{$_.extension -match '\.ps((d|m)*)1' } | 
-    PS>     select -expand fullname | repair-FileEncoding -whatif ;
-    Recurse a sourcecode root, for ps-related files, expand the fullnames and run the set through repair-FileEncoding with whatif
+    PS>     select -expand fullname | repair-FileEncodingTDO -whatif ;
+    Recurse a sourcecode root, for ps-related files, expand the fullnames and run the set through repair-FileEncodingTDO with whatif
     .LINK
     https://github.com/tostka/verb-io
     #>
     # VALIDATORS: [ValidateNotNull()][ValidateNotNullOrEmpty()][ValidateLength(24,25)][ValidateLength(5)][ValidatePattern("some\sregex\sexpr")][ValidateSet("USEA","GBMK","AUSYD")][ValidateScript({Test-Path $_ -PathType 'Container'})][ValidateScript({Test-Path $_})][ValidateRange(21,65)][ValidateCount(1,3)]
     [CmdletBinding()]
-    [Alias('fix-encoding')]
+    [Alias('fix-encoding','repair-BinaryFileTDO','repair-FileEncoding')]
     PARAM(
         [Parameter(Position=0,Mandatory=$false,ValueFromPipeline=$true,HelpMessage="Path to a file or Directory to be checked for encoding or encoding-conversion damage [-Path C:\sc\powershell]")]
-        #[ValidateScript({Test-Path $_ -PathType 'Container'})]
-        [ValidateScript({Test-Path $_ })]
-        [string[]] $Path="C:\sc\powershell",
+            #[ValidateScript({Test-Path $_ -PathType 'Container'})]
+            [ValidateScript({Test-Path $_ })]
+            [string[]] $Path,
+            #"C:\sc\powershell",
         [Parameter(HelpMessage="Array of extensions to be included in checks of Containers (defaults to .ps1|.psm1|.psd1|.cbp) [-IncludeExtentions '.ps1','.psm1','.psd1']")]
-        [array]$IncludeExtentions = @('*.ps1','*.psm1','*.psd1','*.cbp'),
+            [array]$IncludeExtentions = @('*.ps1','*.psm1','*.psd1','*.cbp'),
         [Parameter(HelpMessage="Encoding to be coerced on targeted files (defaults to UTF8, supports:ASCII|BigEndianUnicode|BigEndianUTF32|Byte|Default|OEM|String|Unicode|UTF7|UTF8|UTF32)[-EncodingTarget ASCII]")]
-        [ValidateSet('ASCII','BigEndianUnicode','BigEndianUTF32','Byte','Default','OEM','String','Unicode','UTF7','UTF8','UTF32')]
-        [string]$EncodingTarget= 'UTF8',
+            [ValidateSet('ASCII','BigEndianUnicode','BigEndianUTF32','Byte','Default','OEM','String','Unicode','UTF7','UTF8','UTF32')]
+            [string]$EncodingTarget= 'UTF8',
         [Parameter(HelpMessage="Switch that specifies to also update files with high ascii chars: All matches are reqplaced with the equiv low-asci equivs[-ReplaceChars]")]
-        [switch] $ReplaceChars,
+            [switch] $ReplaceChars,
         [Parameter(HelpMessage="Debugging Flag [-showDebug]")]
-        [switch] $showDebug,
+            [switch] $showDebug,
         [Parameter(HelpMessage="Whatif Flag  [-whatIf]")]
-        [switch] $whatIf=$true
+            [switch] $whatIf=$true
     ) ;
     BEGIN{
+        #region LOCAL_CONSTANTS ; #*------v LOCAL_CONSTANTS v------
+        $PathDefault = "C:\sc\powershell" ; 
+        #endregion LOCAL_CONSTANTS ; #*------^ END LOCAL_CONSTANTS ^------
+        if(-not $Path){
+            # >$Options = @('&Red;Favorite color: Red','&Blue;Favorite color: Blue','&Yellow;Favorite color: Yellow')
+            $Options = @('&Prompt;Path choice: Prompt for',"&Run all $($PathDefault);Process$($PathDefault)")
+            $Answer = Read-InputChoiceTDO -options $Options -title 'Missing Path: Choice?' -Message 'Pick your Path processing choice' -DefaultItem 0 ; 
+            switch -regex ($Answer){
+                'Prompt'{
+                    if($Path = Read-Host "Please target -Path for processing:"){
+                        if(test-path -path $Path){}else{
+                            write-warning "invalid -Path:`n$($Path)! (aborting)" ; 
+                            return ; 
+                        }                        
+                    }else{
+                        write-warning "invalid -Path:`n$($Path)! (aborting)" ; 
+                        return ; 
+                    }
+                }
+                '^Run\sall\s' {
+                    $Path = $PathDefault ; 
+                }
+            } ; 
+        }
+        if($path.count -eq  1){
+            write-host "Single Path item: flipping default -whatif:$false" ; 
+            $whatif = $false ; 
+        } else {
+            write-verbose "Array Path, using default -whatif:$($true)" ; 
+        }
         #*======v SUB MAIN v======
+                
         #region INIT; # ------
         ${CmdletName} = $PSCmdlet.MyInvocation.MyCommand.Name ;
         $Verbose = ($VerbosePreference -eq 'Continue') ;
@@ -176,7 +210,135 @@ function repair-FileEncoding {
             else{ write-warning "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ;
         } ;
         #endregion START-LOG-HOLISTIC #*------^ END START-LOG-HOLISTIC ^------
+        
+      #region FUNCTIONS_INTERNAL ; #*======v FUNCTIONS_INTERNAL v======
 
+        #region READ_INPUTCHOICETDO ; #*------v Read-InputChoiceTDO v------
+        if(-not (gcm Read-InputChoiceTDO -ea 0)){
+            function Read-InputChoiceTDO {
+                <#
+                .SYNOPSIS
+                Read-InputChoiceTDO - Prompts the user with console-based option list and returns the item they specify. Directly emulates Powershell's native prompts.
+                .NOTES
+                Version     : 0.0.2
+                Author      : Todd Kadrie
+                Website     : http://www.toddomation.com
+                Twitter     : @tostka / http://twitter.com/tostka
+                CreatedDate : 2026-06-17
+                FileName    : Read-InputChoiceTDO.ps1
+                License     : (none asserted)
+                Copyright   : (none asserted)
+                Github      : https://github.com/tostka/verb-IO
+                Tags        : Powershell,Input,Prompt,Console
+                AddedCredit : Adam Bertram
+                AddedWebsite: https://4sysops.com/archives/author/adam-bertram/
+                AddedTwitter: URL        
+                REVISIONS   :
+                * 5:29 PM 6/17/2026 init vers
+                .DESCRIPTION
+                Read-InputChoiceTDO - Prompts the user with console-based option list and returns the item they specify. Directly emulates Powershell's native prompts.
+                
+                Inspired by Adam Bertram's blog post: 
+                
+                [Read-Host and the ChoiceDescription class – Prompt for user input in PowerShell – 4sysops - 4sysops.com/](https://4sysops.com/archives/read-host-and-the-choicedescription-class-prompt-for-user-input-in-powershell/)
+
+                Dynamically a custom selection menu using the .NET System.Management.Automation.Host.ChoiceDescription object.        
+
+                The default returned value is the 'Tag', with any ampersand's replaced. 
+                If an entry isn't a semi-colon-delimited list, an integer value is used as the Tag, and the specified string is used as both the HelpMessage and the return value
+
+                .PARAMETER Options
+                Array of Options: Each should be a semi-colon-delimited 'tag;HelpMessage' string: The Tag can have an Ampersand(&) accelerator key[-options @('&Red;Favorite color: Red','&Blue;Favorite color: Blue','&Yellow;Favorite color: Yellow')]
+                .PARAMETER Title
+                Title string to be displayed
+                .PARAMETER message
+                Message to be displayed[-Mesage 'Please select a destination path]
+                .PARAMETER defaultItem
+                INT default selection from -options[-DefaultItem 1]
+                .PARAMETER ReturnInteger
+                Optional switch to return the user-entered integer rather than the resolved entry from the Options list[-ReturnInteger]
+                .INPUTS
+                Does not accept piped input
+                .OUTPUTS
+                System.String
+                .EXAMPLE
+                PS> $Options = @('&Red;Favorite color: Red','&Blue;Favorite color: Blue','&Yellow;Favorite color: Yellow')
+                PS> $Answer = Read-InputChoiceTDO -options $Options -title 'Faviorite color' -Message 'Pick your favorite color' -DefaultItem 1 
+
+                    Faviorite color
+                    Pick your favorite color
+                    [R] Red  [B] Blue  [Y] Yellow  [?] Help (default is "Y"): r
+
+                PS> $Answer
+                        Red
+
+                Demo console option prompt, demonstrating answer if 1st item was chosen, under default behavior (which returns resolved Options entry)        
+                .EXAMPLE
+                PS> $Options = @('Red','Blue','Yellow') ; 
+                PS> $Answer = Read-InputChoiceTDO -options $Options -title 'Faviorite color' -Message 'Pick your favorite color' -DefaultItem 2
+
+                    Faviorite color
+                    Pick your favorite color
+                    [0] 0:Red  [1] 1:Blue  [2] 2:Yellow  [?] Help (default is "2"): 1
+
+                PS> $Answer
+
+                        Blue
+
+                Demo menu using simple string options, without tags, which autoconverts to integer number selections. 
+                .LINK
+                https://github.com/tostka/verb-IO
+                #>
+                [Alias('Read-InputChoice')]
+                [CmdletBinding()]
+                Param(
+                    [Parameter(Position=0,Mandatory=$True,HelpMessage="Array of Options: Each should be a semi-colon-delimited 'tag;HelpMessage' string: The Tag can have an Ampersand(&) accelerator key[-options @('&Red;Favorite color: Red','&Blue;Favorite color: Blue','&Yellow;Favorite color: Yellow')]")]
+                        [array]$Options,
+                    [Parameter(Position=1,HelpMessage="Title string to be displayed[-Title 'Favorite color']")]
+                        [Alias('WindowTitle')]
+                        [string]$Title,
+                    [Parameter(Position=1,HelpMessage="Message to be displayed[-Mesage 'Please pick your favorite color']")]            
+                        [string]$message,
+                    [Parameter(Position=2,HelpMessage="INT default selection from -options[-DefaultItem 1]")]
+                        [int]$defaultItem,
+                    [Parameter(HelpMessage="Optional switch to return the chosedn item ordinal integer rather than the resolved entry from the Options list[-ReturnInteger]")]
+                        [switch]$ReturnInteger
+                ) ;
+                BEGIN{
+                    $i = 0
+                    $mnuOpts = @() ; 
+                    $mnuValues = @() ; 
+                    foreach($Opt in $options){
+                        if($Opt.contains(';')){
+                            $mnuTag,$mnuHelpMsg = $Opt.split(';')
+                            $mnuValues += @($MnuTag -replace '&','')
+                        }else{
+                            $mnuTag = "&$($i):$($Opt)" ; 
+                            $mnuHelpMsg = $Opt
+                            $mnuValues += @($mnuHelpMsg)
+                        } ;                 
+                        $mnuOpts += New-Object System.Management.Automation.Host.ChoiceDescription $mnuTag, $mnuHelpMsg ; 
+                        $i++ ; 
+                    } ; 
+                    $mnuOptions = [System.Management.Automation.Host.ChoiceDescription[]]($mnuOpts)
+                }
+                PROCESS{
+                    $menuSelect = $host.ui.PromptForChoice($title, $message, $mnuOptions, $defaultItem)
+                } ;  # PROC-E
+                END{
+                    
+                    if($ReturnInteger){
+                        $menuSelect | write-output
+                    }else{
+                        $mnuValues[$menuSelect] | write-output
+                    } ;
+                }  # END-E
+            } ;
+        }
+        #endregion READ_INPUTCHOICETDO ; #*------^ END Read-InputChoiceTDO ^------
+        
+        #endregion FUNCTIONS_INTERNAL ; #*======^ END FUNCTIONS_INTERNAL ^======
+        
         #region BANNER ; #*------v BANNER v------
         $sBnr="#*======v $(${CmdletName}): v======" ;
         $smsg = $sBnr ;
@@ -374,4 +536,4 @@ function repair-FileEncoding {
         write-host $stopResults ; 
     } ;  # END-E
 } ; 
-#*------^ END Function repair-FileEncoding ^------
+#endregion REPAIR_FILEENCODINGTDO ; #*------^ END repair-FileEncodingTDO ^------
